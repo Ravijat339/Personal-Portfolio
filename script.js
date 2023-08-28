c0edcd50f8bf38e9b63dc544394f8e3dbe2acf6e
Ravi Prasad Jat <142111488+Ravijat339@users.noreply.github.com>
2023-08-29 01:37:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tablecontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tablecontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
